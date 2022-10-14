5808b749a7bdb7fc7bb1fc490739d9eb8c1bf69a
Cemal Cici <36085842+cemalcici@users.noreply.github.com>
2022-10-14 20:19:23 +0300

    1. eğitim 1. bölüm notu hazırlandı.

---
+++ 01-pythona-giris/01-python-temelleri.py
@@ -0,0 +1,29 @@
+# region Değişkenler
+
+uzunluk = 1.79
+agirlik = 68.7
+beden_kutle_indeksi = agirlik / uzunluk ** 2
+print(beden_kutle_indeksi)
+
+# endregion
+
+# region Python Tipleri
+
+print(type(beden_kutle_indeksi)) # float
+
+haftanin_gunu = 5
+print(type(haftanin_gunu)) # int
+
+x = "Beden kütle indeksi"
+y = 'Bu da çalışıyor'
+print(type(y)) # str
+
+z = True
+print(type(z)) # bool
+
+# Farklı tipler farklı davranış gösterirler!
+
+print(2 + 3) # 5
+print('ab' + 'bc') # abcd
+
+# endregion
